e5c2207a9451bf18cebbbe1042a031cdc9a0da38
Korzhv <vitalikkorzh91@gmail.com>
2022-05-04 15:06:50 +0300

    task 1 and 2 completed

--- src/task_2/ArrayException.java
+++ src/task_2/ArrayException.java
@@ -8,32 +8,33 @@ public class ArrayException {
 
         double[] priceArray = {22.37, 36.35, 20.15, 38.78, 18.16};
         System.out.println("Please, choose the number of the day from offered");
+        for (int i = 0; i < priceArray.length; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
         Scanner scanner = new Scanner(System.in);
-        int userData = getUserInput(scanner);
 
-        System.out.println("Price is: " + priceArray[userData] + "$");
+        getUserInput(scanner, priceArray);
+
     }
 
 
-    public static int getUserInput(Scanner scanner) {
+    public static void getUserInput(Scanner scanner, double[] array) {
         boolean isEnabled = true;
         String userInputData;
         while (isEnabled) {
-
             try {
                 userInputData = scanner.next();
-                Integer.parseInt(userInputData);
+                int data = Integer.parseInt(userInputData);
+                System.out.println("The price is: " + array[data - 1] + "$");
                 isEnabled = false;
             } catch (NumberFormatException exception) {
-                System.out.println();
+                System.out.println("Is not a number.");
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Wrong number.");
                 System.out.println("Please try again");
             }
         }
 
-        return 1;
     }
-
-
 }
